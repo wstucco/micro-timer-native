4dfd6e8d34635d12098ef44e25d76267f88c7c2e
Massimo Ronca <massimo.ronca@leitha.eu>
2022-11-24 11:58:16 +0100

    first satisfying implementation of rust library

--- native/timer/src/lib.rs
+++ native/timer/src/lib.rs
@@ -1,57 +1,54 @@
-use std::panic;
 use std::thread::{self};
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
-use rustler::{Atom, Encoder, Env, LocalPid, OwnedEnv};
-use safina_threadpool::ThreadPool;
+use std::io::Write;
 
-// use scoped_thread_pool::Pool;
+use rustler::{Atom, Encoder, Env, LocalPid, OwnedEnv, Term};
+
+use crossbeam_channel::{unbounded, Receiver, Sender};
 
 #[macro_use]
 extern crate lazy_static;
 
 lazy_static! {
-    // static ref POOL2: Pool = Pool::new(100);
-    static ref POOL: ThreadPool = ThreadPool::new("waiter", 16).unwrap();
+    static ref CHANNEL: (Sender<LocalPid>, Receiver<LocalPid>) = unbounded();
+}
+
+fn load(_env: Env, _: Term) -> bool {
+    thread::spawn(move || loop {
+        if let Ok(pid) = CHANNEL.1.recv() {
+            signal(&pid);
+        }
+    });
+    true
 }
 
 #[rustler::nif]
 fn sleep(env: Env, nanoseconds: u64) -> Atom {
     let pid = env.pid().to_owned();
 
-    match panic::catch_unwind(|| {
-        // let _ = POOL.try_schedule(move || sleep_then_signal(nanoseconds, pid));
-        match POOL.try_schedule(move || sleep_then_signal(nanoseconds, pid)) {
-            Ok(_) => (),
-            Err(_) => {
-                println!("try_schedule failed, spawning a regular thread\r");
-                // thread::spawn(move || sleep_then_signal(nanoseconds, pid));
-            }
-        };
-    }) {
-        Ok(_) => (),
-        Err(error) => {
-            println!("everything failed {:?}\r", error);
-            // thread::spawn(move || sleep_then_signal(nanoseconds, pid));
-        }
-    }
+    thread::spawn(move || sleep_then_signal(nanoseconds, pid));
 
     return rustler::types::atom::ok();
 }
 
 fn sleep_then_signal(duration: u64, pid: LocalPid) -> () {
     do_sleep(Duration::from_nanos(duration));
-    signal(&pid);
+    _ = CHANNEL.0.send(pid);
 }
 
 #[cfg(debug_assertions)]
 fn do_sleep(duration: Duration) {
+    use std::{io, time::Instant};
+
     let start = Instant::now();
 
     thread::sleep(duration);
 
     let elapsed = start.elapsed();
-    println!(
+    let mut lck = io::stdout().lock();
+    _ = writeln!(
+        &mut lck,
         "time elapsed nano: {:?} micro: {:?} millis: {:?}\r",
         elapsed.as_nanos(),
         elapsed.as_micros(),
@@ -61,7 +58,6 @@ fn do_sleep(duration: Duration) {
 
 #[cfg(not(debug_assertions))]
 fn do_sleep(duration: Duration) {
-    println!("Debugging disabled");
     thread::sleep(duration);
 }
 
@@ -71,4 +67,4 @@ fn signal(pid: &LocalPid) {
     msg_env.send_and_clear(pid, |env| (pid, ok).encode(env));
 }
 
-rustler::init!("Elixir.MicroTimer.Native", [sleep]);
+rustler::init!("Elixir.MicroTimer.Native", [sleep], load = load);
